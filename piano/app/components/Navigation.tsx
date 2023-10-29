cf5018fedbe55386408a7c2a063e4709cbe4a2e8
Ravvsky <pawel.rawsk@gmail.com>
2023-10-29 17:35:39 +0100

    feat: create Navigation component

---
+++ piano/app/components/Navigation.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Logo from "../assets/white.svg";
+const Navigation = () => {
+  return (
+    <nav className="flex h-[4.4rem] justify-between bg-navy">
+      <Logo className="h-[4rem] w-auto" />
+    </nav>
+  );
+};
+
+export default Navigation;
