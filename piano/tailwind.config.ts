199c18083fdeda92815c8ee0893e5beda1a9fb32
Ravvsky <pawel.rawsk@gmail.com>
2023-10-29 14:19:33 +0100

    chore: add colors to tailwind config

--- piano/tailwind.config.ts
+++ piano/tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: {
+          100: "#ffffff",
+          200: "#ccc",
+        },
+      },
+    },
+    colors: {
+      white: "#F0F0F0",
+      black: "#1C1C1A",
+      navy: "#154151",
+      red: "944038",
+      brown: "381815",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
