d6ba1f0414b8ed2393a0352403ed9528de5880e4
Ravvsky <pawel.rawsk@gmail.com>
2023-10-31 15:11:03 +0100

    chore: update next config to handle svgr

--- piano/next.config.js
+++ piano/next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
 
-module.exports = nextConfig
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
