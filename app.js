40cb06e5073ed27bbbf78d4af84665bb15e2b83a
AkbarAckerman <akbarkhon0520@gmail.com>
2023-11-07 00:50:23 +1400

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import PianoRoll from './pianoroll.js';
 
 class PianoRollDisplay {
-  constructor(csvURL) {
-    this.csvURL = csvURL;
+  constructor() {
     this.data = null;
   }
 
@@ -18,42 +17,34 @@ class PianoRollDisplay {
     }
   }
 
-  preparePianoRollCard(rollId) {
+  preparePianoRollCard(rollId, svg) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('piano-roll-card');
-
-    // Create and append other elements to the card container as needed
     const descriptionDiv = document.createElement('div');
     descriptionDiv.classList.add('description');
     descriptionDiv.textContent = `This is a piano roll number ${rollId}`;
     cardDiv.appendChild(descriptionDiv);
-
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.classList.add('piano-roll-svg');
-    svg.setAttribute('width', '80%');
-    svg.setAttribute('height', '150');
-
-    // Append the SVG to the card container
     cardDiv.appendChild(svg);
-
-    return { cardDiv, svg }
+    return cardDiv;
   }
 
   async generateSVGs() {
     if (!this.data) await this.loadPianoRollData();
     if (!this.data) return;
-    
+
     const pianoRollContainer = document.getElementById('pianoRollContainer');
     pianoRollContainer.innerHTML = '';
-    for (let it = 0; it < 20; it++) {
+
+    for (let it = 0; it < 10; it++) {
       const start = it * 60;
       const end = start + 60;
       const partData = this.data.slice(start, end);
-
-      const { cardDiv, svg } = this.preparePianoRollCard(it)
-
-      pianoRollContainer.appendChild(cardDiv);
-      const roll = new PianoRoll(svg, partData);
+      const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      svg.classList.add('piano-roll-svg');
+      svg.setAttribute('width', '80%');
+      svg.setAttribute('height', '150');
+      pianoRollContainer.appendChild(this.preparePianoRollCard(it, svg));
+      new PianoRoll(svg, partData);
     }
   }
 }
